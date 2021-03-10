aea0fd6b5f8e28cf545b80f42d11980e9037cf39
acul009 <acul@localhost.localdomain>
2021-03-10 20:58:04 +0100

    Added comment for later

--- php/core/ProtectedSingletonException.php
+++ php/core/ProtectedSingletonException.php
@@ -5,6 +5,8 @@ namespace core;
 /**
  * Thrown when trying to create a protected Singleton, that has already been created
  *
+ * This should probably instantly exit
+ *
  * @author acul
  */
 class ProtectedSingletonException extends BadMethodCallException {
