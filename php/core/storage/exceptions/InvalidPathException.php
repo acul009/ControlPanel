f1fcb35babedd6e3a771f4f8d41de83239218c41
acul009 <acul@localhost.localdomain>
2021-06-21 17:38:17 +0200

    Added InvalidPathException

---
+++ php/core/storage/exceptions/InvalidPathException.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace core\storage\exceptions;
+
+use UnexpectedValueException;
+
+/**
+ * <pre>
+ * Thrown when a path given to the Filesystem Api tries to break out of the data directory
+ * or is invalid.
+ * </pre>
+ * @author acul
+ */
+class InvalidPathException extends UnexpectedValueException {
+
+  public function __construct(string $path = "", int $code = 0, \Throwable $previous = NULL) {
+    $message = 'The given Path "' . $path . '" is invalid or forbidden.';
+    parent::__construct($message, $code, $previous);
+  }
+
+}
