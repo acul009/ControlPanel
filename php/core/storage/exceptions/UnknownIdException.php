ad265524f59c4cfa391fdf398625e49cbca89102
acul009 <acul@localhost.localdomain>
2021-06-21 17:37:38 +0200

    Added UnknownIDException

---
+++ php/core/storage/exceptions/UnknownIdException.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace core\storage\exceptions;
+
+use UnexpectedValueException;
+
+/**
+ * <pre>
+ * Thrown when trying to load an ID that doesn't exist (yet).
+ * </pre>
+ *
+ * @author acul
+ */
+class UnknownIdException extends UnexpectedValueException {
+
+  public function __construct(int $id, int $code = 0, \Throwable $previous = NULL) {
+    $message = 'The given Id "' . $id . '" could not be found.';
+    parent::__construct($message, $code, $previous);
+  }
+
+}
