1bce0d68c645536bc172ed817fd1844fcb484429
acul009 <acul@localhost.localdomain>
2021-09-04 02:25:00 +0200

    added missing return statement

--- php/acul009/ControlPanel/loadCore.php
+++ php/acul009/ControlPanel/loadCore.php
@@ -8,7 +8,8 @@ set_error_handler(function (int $severity, string $message, string $filename, in
 const PHP_SUBDIR = 'php';
 spl_autoload_register(function (string $className) {
     if (!str_starts_with($className, '\acul009\ControlPanel\\'))
-        static $phpDir;
+        return;
+    static $phpDir;
     if (!isset($phpDir)) {
         $phpDir = dirname(getcwd()) . DIRECTORY_SEPARATOR . PHP_SUBDIR . DIRECTORY_SEPARATOR;
     }
